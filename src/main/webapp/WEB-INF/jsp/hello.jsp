ed222f739a3011c91904d8f123ce23b01475b35e
Andrzej Luda <andrzejluda@gmail.com>
2019-07-28 15:10:10 +0200

    initial commit

---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: W530
+  Date: 2019-07-28
+  Time: 14:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello</h1>
+
+</body>
+</html>
---
+++ src/test/java/pl/luda/eventer/EventerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.luda.eventer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
